eb5a426c3bea13d083c594f37b0d01f2d85e7788
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-18 16:24:30 +0530

    Create vowel or constant.c

---
+++ vowel or constant.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ char a
+ int upvowel,lowvowel
+ printf("%d,enter the alphapet");
+ scanf(%c,&a);
+ if(upvowel||lowvowel)
+ printf("%d,enter the vowel",a);
+ else
+ printf("%d,enter the constant",a);
+ }
