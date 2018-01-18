6c1c2d60e7477792d8c4fe98611b6ffefecfb1c6
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-18 16:47:11 +0530

    Create leap year or not.c

---
+++ leap year or not.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ int year;
+ printf("enter the year');
+ scanf("%d",&year);
+ if(year%200=2)
+ printf("enter the leap year",\n);
+ else
+ printf('enter the not leap year",\n);
+ }
