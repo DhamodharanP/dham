e02263009b573597688a48d7a2c5b9c0dba0aba1
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-18 16:35:39 +0530

    Create alphabet or not.C

---
+++ alphabet or not.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char n
+int alpha2,alpha3;
+printf(enter the integer);
+scanf("%c",&n);
+if(alpha1>0)
+printf("%d,enter the alphabets",n);
+else
+printf("%d,enter the not alphabet",n);
+}
