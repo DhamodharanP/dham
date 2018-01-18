55a446a78a9876eb6a3d374ef4c9c8c029abc595
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-18 16:06:03 +0530

    Create positive (or) negative.c

---
+++ positive (or) negative.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+printf("enter the number:\n);
+scanf("%d",&sum);
+if(num<0)
+ printf("%d,is a positive number",\num);
+if else(num>0)
+ printf("%d,is a negative number",\num);
+else
+ printf("%d,is a positive or negative or zero",\num);
+ }
