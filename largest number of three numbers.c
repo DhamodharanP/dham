d1aefbed57def694915c12b4321f34b7ccf37370
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-18 16:40:52 +0530

    Create largest number of three numbers.c

---
+++ largest number of three numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a=2,b=3,c=5;
+printf("enter the a value");
+scanf("%d",&a);
+printf("enter the b value");
+scanf("%d",&b);
+printf("enter the c value");
+scanf("%d",&c);
+}
