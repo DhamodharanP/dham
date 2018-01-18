d509f5fdd3b47e5d417a42e14749abc476b234d4
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-18 16:14:16 +0530

    Create even(or)odd.c

---
+++ even(or)odd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ int number
+ printf("%d,enter the integer");
+ scanf("%d",&number);
+ if(num%2=0)
+ printf("%d,enter the odd number",\n);
+ else(num%2>0)
+ printf("%d,enter the even number",\n);
+ }
