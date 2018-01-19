4508fb3645c0ca4a446653cc3eab26cb4753af46
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-19 08:45:36 +0000

    Create prime or not.c

---
+++ prime or not.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int prime,a=56;
+printf("enter the integer");
+scanf("%d",&prime);
+if(n%1=6)
+printf("enter the prime");
+else
+printf("enter the prime or not);
+}
