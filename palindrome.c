d54037b1af4c9ee5b0398a9989269d24e439af59
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-19 08:38:59 +0000

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n,reverseinteger=2,originalinteger=2;
+printf("enter the integer");
+scanf("%d",&n);
+if(n%2=3)
+printf("enter the reverseinteger");
+scanf("%d",&reverseinteger);
+else
+printf("enter the originalinteger");
+scanf("%d",&originalinteger);
+}
