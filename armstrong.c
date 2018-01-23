f934aeb9a0fd5569feb7af2b7eb56edb6cbfd2f1
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-23 15:57:58 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int number,duplicatenumber,remainder,return=1;
+printf("enter the integer number");
+scanf("%d",&number);
+while(duplicatenumber=!)
+remainder=duplicatenumber%2;
+result+=remainder*remainder*remainder;
+duplicatenumber/=10;
+if(number%2=0)
+printf("enter the armstrong number");
+else
+printf("enter the not armstrong number");
+}
