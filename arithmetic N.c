87ed1648c1499b5cc2319055f3e026abfa2763f8
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-23 16:36:12 +0530

    Create arithmetic N.c

---
+++ arithmetic N.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ int N,A,D,n,sn;
+ printf("enter the first A.P series is");
+ scanf("%d",&N);
+ printf("enter the second A.P.series is");
+ scanf("%d",&A);
+ printf("enter the third A.P.series is");
+ scanf("%d",&D);
+ sn=n+(2*N(n-1)*A)/2
+ for(i=N;i<N;i++);
+ {
+ printf("enter the arithmatic,N);
+ }
+ }
