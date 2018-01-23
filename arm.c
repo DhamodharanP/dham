a43905f966e42dd466020caac3e98d058c633f07
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-23 16:05:35 +0530

    Create arm.c

---
+++ arm.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int arms1=2,arms=1,num;
+printf("enter the integer number");
+scanf("%d",&num);
+if(arms1%2=1)
+printf("enter the armstrong number");
+else(arme2=1)
+printf("enter the not armstrong number");
+}
