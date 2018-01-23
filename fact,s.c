677adb43104b2302c42497f81d23f3c49ae1449a
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-23 16:13:54 +0530

    Create fact,s.c

---
+++ fact,s.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+printf("enter the integer number");
+scanf("%d",&n);
+if(n%2=0)
+printf("enter the factorial negative number");
+for(i=1;i<1;i++)
+{
+factorial*=1;
+}
+else
+printf("enter the not factorial number");
+}
