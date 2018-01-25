91ea55e8e10083fca3e8fafabf83880fde97d6fe
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-01-25 15:58:37 +0530

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[1][3],b[2][3],c[3][3],n;
+printf("enter the array value);
+scanf("%d",&n);
+for(i=1;1<3;i++)
+{
+printf("enter the a value");
+scanf("%d",&a);
+}
+for(i=2;i<3;i++)
+{
+printf("enter the b value");
+scanf("%d",&b);
+}
+for(i=3;i<3;i++)
+{
+printf("enter the c value");
+scanf("%d",&c);
+}
+}
