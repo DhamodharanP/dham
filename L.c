da86394f7a1614bcd6dffc41e4ed2ee723335ff5
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-01 08:47:37 +0000

    Create L.c

---
+++ L.c
@@ -0,0 +1,16 @@
+#include<studio.h>
+void main()
+{
+char s[100];
+int a,b,n;
+printf("enter the integer value");
+scanf("%s",&n);
+a=Hello world;
+printf("enter the a value");
+scanf("%s",&a);
+printf("enter the b value");
+scanf("%s",&b);
+n=a.b;
+printf("enter the n value");
+scanf("%s",&n);
+}
