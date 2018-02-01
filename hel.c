adac0ff48ec95d71448f53c0ebfed4d1b0d96169
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-01 08:05:11 +0000

    Create hel.c

---
+++ hel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char a="Hello World";
+int count=0;
+printf("enter the string");
+scanf("%s",&a);
+for(i=0;a[i]='\0';i++)
+{
+count++;
+}
+printf("enter the count");
+}
