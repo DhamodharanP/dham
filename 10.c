7eb177ef72753870d745ae0b5d27dcf1a5c85ff7
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-02 14:11:01 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a[10];
+int i;
+int max;
+printf("enter ten values");
+for(i=0;i<10;i++)
+{
+scanf("%d",a[i]);
+}
+max=a[0];
+for(i=o;i<10;i++)
+{
+if(a[i>max)
+{
+max=a[i];
+}
+printf("enter the 10 valuess");
+}
+return 0;
+}
