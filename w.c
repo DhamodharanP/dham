4899497d84e260d202614e309f8ad95b1daac2b4
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-09 14:31:53 +0530

    Create w.c

---
+++ w.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,num=1;
+printf("enter the print number");
+scanf("%d",&n);
+while(num!=0)
+{
+switch(num!=0)
+{
+case0:
+  printf("zero");
+  break;
+case1:
+  printf("one");
+  break;
+}
+}
+}
