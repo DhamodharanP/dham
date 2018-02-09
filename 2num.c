4e69c102b373761384708a5d0a31fa00de306f1b
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-09 15:01:23 +0530

    Create 2num.c

---
+++ 2num.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int number1=12,number2=3;
+printf("enter the integer value");
+scanf("%d%d",&number1number2);
+if(number1%2=0)
+{
+printf("%d",odd);
+printf("%d",even);
+}
+
