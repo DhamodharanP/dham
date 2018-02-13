36efc769c80540ed7f4e18b4dede8a1fe082ef99
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-13 16:47:09 +0530

    Create fibt,.c

---
+++ fibt,.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,i,t1=5,next term;
+printf("enter the no of terms");
+scanf("%d',&n);
+printf("fibonnacci series");
+for(i=0;i<=5;++i);
+{
+printf("%d",t1);
+next term=t1;
+}
+}
