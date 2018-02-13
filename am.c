eeb543cd14834d738560631b294662e64b976c8e
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-13 16:39:52 +0530

    Create am.c

---
+++ am.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a[10],num1,.,num10,max,i;
+printf("enter the array");
+scanf("%d",&a);
+if(num1>num2)
+{
+if(num1>num3)
+{
+max=num3;
+}
+if(num2>num3)
+{
+if(num2>num4)
+{
+max=num4;
+}
+else
+{
+printf("enter the a[i]");
+i=num1,...,num10;
+}
+}
+}
