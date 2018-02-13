1a694329a04efeb2c87b074d0a80bda6b04cd686
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-13 16:04:19 +0530

    Create al.c

---
+++ al.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+char str[10];
+int n,num,i;
+printf("enter the integer value");
+scanf("%d",&num);
+for(i=0;i<=10;i++)
+{
+num=i;
+}
+printf("enter the alphabets");
+}
