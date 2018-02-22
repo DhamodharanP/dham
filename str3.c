23079599a599f8c1cc022f809fb877c7717b3e06
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-22 16:02:50 +0530

    Create str3.c

---
+++ str3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s[20],a[50];
+printf("the string is");
+scanf("%s",&s);
+int k;
+scanf("%s",&k);
+int i;
+for(i=0;i<k;i++)
+{
+s[i]=a[i];
+}
+printf("\n,%s",string);
+}
