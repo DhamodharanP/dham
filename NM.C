1ad10e120df0698f688e4d695b59c46535ce80ef
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-27 15:51:51 +0530

    Create NM.C

---
+++ NM.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int N=9,M=2,n;
+printf("enter the integer");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("enter the odd number",N);
+else
+{
+printf("enter the even number",M);
+}
+}
