33de308a57e9a9a4cced43ec11fb973d6aba1824
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-27 16:35:33 +0530

    Create vgh.c

---
+++ vgh.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char num,n;
+printf("enter the integer value");
+scanf("%d",n);
+while(num%2==0)
+{
+numb++;
+}
+printf("enter the nearest greatest value%2",num);
+}
