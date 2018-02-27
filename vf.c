f163b5c897598f6344325e1045e3e381038d3385
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-27 16:31:59 +0530

    Create vf.c

---
+++ vf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a=10,b=3,number;
+printf("enter the integer value");
+scanf("%d",&num);
+if(num%2=0)
+{
+printf("enter the odd number",b);
+}
+else
+{
+printf('enter the even number",a);
+]
+}
