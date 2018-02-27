413531230ada00dc4191198f6fa9a7bb46deade0
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-02-27 16:22:15 +0530

    Create 00.c

---
+++ 00.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num,n;
+printf("enter the integer number");
+scanf("%d",&num);
+while(num%10==0)
+{
+numb++;
+}
+printf("enter the nearest greatest 10",num);
+}
