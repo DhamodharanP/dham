d21426b93b715703f5d5fdaad9b5af8c07451de2
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-01 16:07:35 +0530

    Create el.c

---
+++ el.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char line[20];
+int btabb;
+printf('enter the line string");
+scanf("%s",&line);
+if(line[i]==b;line[i]==t;line[i]==a;line[i]==b;line[i]==b)
+{
+ ++vowels;
+ vowel=yes;
+ printf("%s",vowel);
+}
+}
