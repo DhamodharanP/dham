1333928d922efacb706ee28bbb56ff000c0554e7
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-13 16:07:50 +0530

    Create kg1.c

---
+++ kg1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int a=26,num;
+ printf("enter the number");
+ scanf("%d",&a);
+ num=2;
+ printf("%d",num);
+ if(num*26==0)
+ {
+ printf("no");
+ }
+ else
+ {
+ printf("yes");
+ }
+ return 0;
+}
