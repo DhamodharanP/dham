d89b0b56b5671c518f9b683ff15571282e21bd19
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-13 16:18:05 +0530

    Create sq.c

---
+++ sq.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+int a=5,b=5,n;
+printf("enter a value");
+scanf("%d",&n);
+printf("enter b value");
+scanf("%d",&n);
+for(a=0;a<=n;a++)
+{
+if(n==a*a)
+{
+printf("yes");
+return 0;
+}
+else
+printf("no");
+return 0;
+}
+for(b=0;b<=n;b++)
+{
+if(n==b*b)
+{
+printf("yes);
+return0;
+}
+else
+{
+printf("no");
+return 0;
+}
+}
