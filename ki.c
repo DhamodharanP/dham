fd7c7c3b33ccddbad86f767be7848ecd93d332c8
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-13 15:47:52 +0530

    Create new file

---
+++ ki.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int num;
+ printf("enter the number");
+ scanf("%d",&num);
+ if(num*13==0)
+ {
+ printf("number is multiple by 13");
+ }
+ else
+ {
+ printf("number is not multiple by 13");
+ }
+ return 0;
+}
