1ae17f7a5fa4391293e158a0237f699dc9974f5c
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-17 10:59:35 +0000

    Create oh.c

---
+++ oh.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a=110,b=100,c=99,d=10,e=2147483647,f=2147483646,S,n;
+printf("enter the number");
+scanf("%d",&n);
+printf("%d",a);
+printf("%d",b);
+printf("%d",c);
+printf("%d",d);
+printf('%d',e);
+printf("%d",f);
+S=a/b;
+S=c%d;
+S=e/e;
+S=f%e;
+printf("%d",S);
+}
