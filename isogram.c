cda6c4c86da8cbea661b14ec9a8d9ce7fd1197ac
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-17 09:10:31 +0000

    Create isogram.c

---
+++ isogram.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[10];
+char a="Helloworld";
+scanf("%s",&a);
+int n,i,d;
+printf("enter the number");
+scanf("%s",&n);
+for(i=0;i<=n;i++)
+{
+ d=d+i;
+if(n%2==0)
+{
+ printf("%d",yes);
+}
+else
+{
+printf("%d",no);
+}
+}
+return 0;
+}
