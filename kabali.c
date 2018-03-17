aab5ef489bd497d31c5fa4487f57e02d7a35f6df
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-17 10:17:29 +0000

    Create kabali.c

---
+++ kabali.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a=10,b=12,c=14,d=100,e=200,n,A,B,C;
+printf("enter the number");
+scanf("%d",&n);
+printf("%d",a);
+printf("%d",b);
+Printf("%d\n%d\n%d",cde);
+A=a-b;
+B=a-c;
+C=d-e;
+printf("%d\n%d\n%d",ABC);
+return 0;
+}
