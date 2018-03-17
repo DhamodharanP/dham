cc11adbeb0592ad8a5146af11947a219ec140d10
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-17 09:42:12 +0000

    Create der.c

---
+++ der.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.c>
+int main()
+{
+char a="5GUVI12";
+int n,del;
+printf("enter the number");
+scanf("%d",&n);
+printf("%d",a);
+del(a)=GUVI;
+printf("%d",del(a));
+}
