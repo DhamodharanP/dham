1a1cfb13238d8bbc0ce43a0858acf74f7d354e88
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-17 10:36:09 +0000

    Create are.c

---
+++ are.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float length=1.626,breadth=2.31,area;
+printf("\n enter the length value%d");
+scanf("%d',&length);
+printf("enter the breadth value%d");
+scanf("%d",&breadth);
+area=length*breadth;
+printf("%d",area);
+}
