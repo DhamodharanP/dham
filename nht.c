cb9857d20b932152d7a4429a55f3559861c50da0
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-20 16:31:07 +0530

    Update nht.c

--- nht.c
+++ nht.c
@@ -3,12 +3,12 @@
 int main()
 {
 int L,B,H;
-int surface area,volume;
+int surfacearea,volume;
 printf("enter the L B H of the cuboid");
 scanf("%f%f%f",&L,&B,&H);
-surface area=2*(L*B+B*H+H*L);
+surfacearea=2*(L*B+B*H+H*L);
 volume=L*B*H;
-printf("surface area of cuboids:%.3f",surface area);
+printf("surface area of cuboids:%.3f",surfacearea);
 printf("\n volume of cuboids:%.3f",volume);
 return 0;
 }
