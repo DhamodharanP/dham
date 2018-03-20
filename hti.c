ffb48855134b41c1e66e0e043435225fafd8a488
DhamodharanP <31504001+DhamodharanP@users.noreply.github.com>
2018-03-20 15:50:54 +0530

    Create hti.c

---
+++ hti.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a=110,b=100,c=99,d=10,e=2147483647,f=2147483646,A,E,D,H,n;
+printf("enter the integer value");
+scanf("%d",&n);
+printf("%d",a);
+printf("%d",b);
+printf("%d",c);
+printf("%d",d);
+printf("%d",e);
+printf("%d",f):
+A=a\b;
+E=c%d;
+D=e\e;
+H=f\e;
+printf("%d",A);
+printf("%d",E);
+printf("%d",D);
+printf("%d",H);
+}
